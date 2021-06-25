0ac88acc886ce44518bf2d0e2fe4099e0f958c99
github username <github email address>
2021-06-24 21:06:35 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -39,8 +38,8 @@ ActiveRecord::Schema.define(version: 20140710181927) do
   end
 
   create_table "reservations", force: :cascade do |t|
-    t.date     "checkin"
-    t.date     "checkout"
+    t.date     "check_in"
+    t.date     "check_out"
     t.integer  "listing_id"
     t.integer  "guest_id"
     t.datetime "created_at",                     null: false
@@ -61,7 +60,7 @@ ActiveRecord::Schema.define(version: 20140710181927) do
     t.string   "name"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
-    t.boolean  "host",       default: false
+    t.boolean  "is_host",    default: false
   end
 
 end
